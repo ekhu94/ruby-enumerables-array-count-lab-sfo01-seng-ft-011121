accf6a7bc77c8a6ff901c7cf6bdf8ad894baed6f
Erik Huang <erikhuangdrums@gmail.com>
2021-01-04 20:54:36 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,11 +1,3 @@
 def count_strings(arr)
-  count = 0
-  arr.each { |ele| count += 1 if ele.is_a? String }
-  count 
-end
-
-def count_empty_strings(arr)
-  count = 0
-  arr.each { |ele| count += 1 if ele == "" }
-  count
+  arr.count { |ele| ele.is_a? String }
 end
